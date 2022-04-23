3fedcd7117bcf071764c12d67941bcfd928d3859
hafizhah wihdatul <80366029+fidhaaa@users.noreply.github.com>
2022-04-23 23:43:30 +0700

    Add files via upload
    yuk ini

---
+++ lib/HomePage.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:belajar_1/screen/chatPage.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({ Key? key }) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ChatPage(),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 1,
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.grey.shade600,
+        selectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        unselectedLabelStyle: TextStyle(fontWeight: FontWeight.w600),
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat),
+            label: "Chat",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.location_on),
+            label: "Location",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.align_vertical_bottom),
+            label: "Activity",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_box),
+            label: "Profile",
+          ),
+
+
+
+        ],
+      )
+    );
+  }
+}
+
+
+    
+
